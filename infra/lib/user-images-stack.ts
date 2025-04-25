02249a05915ae9832034cfc30046086c3d0da022
ivanneto02 <ivan.a.neto@hotmail.com>
2025-04-25 01:44:32 -0700

    Partial update to infrastructure

--- infra/lib/user-images-stack.ts
+++ infra/lib/user-images-stack.ts
@@ -1,39 +1,62 @@
-import { Stack, StackProps } from "aws-cdk-lib";
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as path from "path";
 
-const s3 = require("aws-cdk-lib/aws-s3");
-const lambda = require("aws-cdk-lib/aws-lambda");
-
-/*
- *	Stack to add users to the site:
+/* STACK MEANT TO STORE IMAGES FOR THIS WEBSITE
  *
- *	S3 Bucket responsible for holding the users
- *		- Triggers lambda function to index user via DynamoDB database
- *	
- *	DynamoDB index table
- *		- username: string
- *		- date: string
- *		- email?: date
+ * userImagesBucket: AWS S3 Bucket
  *
- *	API Gateway
- *		- Front for users clicking on sign-up button
- *
- *	Lambda1
- *		- Gets trigged by S3 bucket
- *
- * 	Lambda2
- * 		- Called to check if username is unique
- *
+ * cloudFrontDistribution: AWS CloudFront
  * */
-export class AlphanumericDetectorImagesStack extends Stack {
-    /**
-     *
-     * @param {Construct} scope
-     * @param {string} id
-     * @param {StackProps=} props
-     */
+export class AlphanumericDetectorImagesStack extends cdk.Stack {
+        /**
+         * @param {Construct} scope
+         * @param {string} id
+         * @param {StackProps=} props
+         */
+        constructor(scope: Construct, id: string, props: cdk.StackProps) {
+                super(scope, id, props);
 
-    // Blank stack for now
+                /* S3 Bucket will store all the images that the users upload. Images are stored under the path ROOT/user/[imagename].png. All images are publicly accessible. */
+                const userImagesBucket = new s3.Bucket(this, "PortfolioWebsiteImagesBucket", {
+                        publicReadAccess: false,
+                        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+                        accessControl: s3.BucketAccessControl.PRIVATE,
+                        removalPolicy: cdk.RemovalPolicy.RETAIN,
+                });
 
+                /* Allows browser access to the bucket */
+                userImagesBucket.addCorsRule({
+                        allowedOrigins: ["*"],
+                        allowedMethods: [s3.HttpMethods.GET],
+                        allowedHeaders: ["*"],
+                });
+
+                // CloudFront will cache these images and distribute them appropriate. Since
+                // this service comes with AWS Shield Standard included, it should prevent DDoS attacks. Images are distributed publicly. ANYONE CAN ACCESS THEM BECAUSE THEY ARE NOT MEANT TO BE HIDDEN.
+                const cloudFrontDistribution = new cloudfront.Distribution(this, "PortfolioWebsiteImagesCDN", {
+                        defaultBehavior: {
+                                origin: origins.S3BucketOrigin.withOriginAccessControl(userImagesBucket),
+                                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                                allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+                                cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+                                responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS,
+                        }
+                });
+
+                // TODO: We need a dynamoDB table to index the images that each user has.
+                //
+                // TODO: Add API gateway endpoints for executing lambda functions to index a user addition
+                //
+                // TODO: Add lambda functions that index user addition
+                //
+                // TODO: figure out how the indexing is going to work. Do some research on how people usually index their images in S3 with DynamoDB table.
+
+        };
 };
--- infra/lib/users-stack.ts
+++ infra/lib/users-stack.ts
@@ -3,7 +3,6 @@ import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as path from "path";
 
 /*
  *	Stack to add users to the site:
@@ -27,74 +26,74 @@ import * as path from "path";
  *
  * */
 export class AlphanumericDetectorUsersStack extends cdk.Stack {
-    /**
-     *
-     * @param {Construct} scope
-     * @param {string} id
-     * @param {cdk.StackProps=} props
-     */
+        /**
+         *
+         * @param {Construct} scope
+         * @param {string} id
+         * @param {cdk.StackProps=} props
+         */
 
-    constructor(scope: Construct, id: string, props: cdk.StackProps) {
-        super(scope, id, props);
+        constructor(scope: Construct, id: string, props: cdk.StackProps) {
+                super(scope, id, props);
 
-        /* DynamoDB table to store the users */
-        const userDataTable: dynamodb.TableV2 = new dynamodb.TableV2(this, "AlphanumericDetectorUserDataTable", {
-            partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
-            contributorInsights: true,
-            tableClass: dynamodb.TableClass.STANDARD,
-        });
-        /* --- */
+                /* DynamoDB table to store the users */
+                const userDataTable: dynamodb.TableV2 = new dynamodb.TableV2(this, "AlphanumericDetectorUserDataTable", {
+                        partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
+                        contributorInsights: true,
+                        tableClass: dynamodb.TableClass.STANDARD,
+                });
+                /* --- */
 
-        /* Lambda function adds users to the dynamodb table */
-        const addUserData: lambda.Function = new lambda.Function(this, "AlphanumericDetectorAddUserData", {
-            code: lambda.Code.fromAsset("lib/userStackFunctions/addUserDataLambda"),
-            runtime: lambda.Runtime.NODEJS_LATEST,
-            handler: 'index.handler',
-            environment: {
-                TABLE_NAME: userDataTable.tableName, // need for writing
-            }
-        });
-        // Make sure lambda function has permission to write
-        userDataTable.grantWriteData(addUserData);
-        /* --- */
+                /* Lambda function adds users to the dynamodb table */
+                const addUserData: lambda.Function = new lambda.Function(this, "AlphanumericDetectorAddUserData", {
+                        code: lambda.Code.fromAsset("lib/userStackFunctions/addUserDataLambda"),
+                        runtime: lambda.Runtime.NODEJS_LATEST,
+                        handler: 'index.handler',
+                        environment: {
+                                TABLE_NAME: userDataTable.tableName, // need for writing
+                        }
+                });
+                // Make sure lambda function has permission to write
+                userDataTable.grantWriteData(addUserData);
+                /* --- */
 
-        /* API Gateway to front the lambda function to add user data */
-        const addUserDataGatewayEndpoint: apigateway.LambdaRestApi = new apigateway.LambdaRestApi(this, "AlphanumericDetectorAddUserDataEndpoint", {
-            handler: addUserData,
-            restApiName: "AlphanumericDetectorAddUserDataRestAPI",
-            defaultCorsPreflightOptions: { // need to allow cors
-                allowOrigins: apigateway.Cors.ALL_ORIGINS,
-                allowMethods: ["POST"],
-                allowHeaders: ["Content-Type"],
-            }
-        });
-        /* --- */
+                /* API Gateway to front the lambda function to add user data */
+                const addUserDataGatewayEndpoint: apigateway.LambdaRestApi = new apigateway.LambdaRestApi(this, "AlphanumericDetectorAddUserDataEndpoint", {
+                        handler: addUserData,
+                        restApiName: "AlphanumericDetectorAddUserDataRestAPI",
+                        defaultCorsPreflightOptions: { // need to allow cors
+                                allowOrigins: apigateway.Cors.ALL_ORIGINS,
+                                allowMethods: ["POST"],
+                                allowHeaders: ["Content-Type"],
+                        }
+                });
+                /* --- */
 
-        /* Lambda2 lambdaUniqueUserNameCheck - Checks if username is already present in the DynamoDB table */
-        const uniqueUserNameCheckLambda: lambda.Function = new lambda.Function(this, "AlphanumericDetectorUniqueUserNameCheck", {
-            code: lambda.Code.fromAsset("lib/userStackFunctions/uniqueUserNameCheckLambda"),
-            runtime: lambda.Runtime.NODEJS_LATEST,
-            handler: 'index.handler',
-            environment: {
-                TABLE_NAME: userDataTable.tableName, // need for reading
-            }
-        });
-        // Make sure lambda function has permission to read
-        userDataTable.grantReadData(uniqueUserNameCheckLambda);
-        /* --- */
+                /* Lambda2 lambdaUniqueUserNameCheck - Checks if username is already present in the DynamoDB table */
+                const uniqueUserNameCheckLambda: lambda.Function = new lambda.Function(this, "AlphanumericDetectorUniqueUserNameCheck", {
+                        code: lambda.Code.fromAsset("lib/userStackFunctions/uniqueUserNameCheckLambda"),
+                        runtime: lambda.Runtime.NODEJS_LATEST,
+                        handler: 'index.handler',
+                        environment: {
+                                TABLE_NAME: userDataTable.tableName, // need for reading
+                        }
+                });
+                // Make sure lambda function has permission to read
+                userDataTable.grantReadData(uniqueUserNameCheckLambda);
+                /* --- */
 
-        /* API Gateway to front the unique users Lambda2 function */
-        const uniqueUserNameCheckGatewayEndpoint: apigateway.LambdaRestApi = new apigateway.LambdaRestApi(this, "AlphanumericDetectorUniqueUserNameCheckEndpoint", {
-            handler: uniqueUserNameCheckLambda,
-            restApiName: "AlphanumericDetectorUniqueUserNameCheckRestAPI",
-            defaultCorsPreflightOptions: {
-                allowOrigins: apigateway.Cors.ALL_ORIGINS,
-                allowMethods: ["GET"],
-                allowHeaders: ["Content-Type"],
-            }
-        });
-        /* --- */
+                /* API Gateway to front the unique users Lambda2 function */
+                const uniqueUserNameCheckGatewayEndpoint: apigateway.LambdaRestApi = new apigateway.LambdaRestApi(this, "AlphanumericDetectorUniqueUserNameCheckEndpoint", {
+                        handler: uniqueUserNameCheckLambda,
+                        restApiName: "AlphanumericDetectorUniqueUserNameCheckRestAPI",
+                        defaultCorsPreflightOptions: {
+                                allowOrigins: apigateway.Cors.ALL_ORIGINS,
+                                allowMethods: ["GET"],
+                                allowHeaders: ["Content-Type"],
+                        }
+                });
+                /* --- */
 
-    }
+        }
 };
 
