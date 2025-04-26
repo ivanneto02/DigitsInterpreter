87206979c29f1a78cfa81c0b29d526cc42b8a294
Ivan Neto <ivan.a.neto@hotmail.com>
2025-04-25 18:10:18 -0700

    Working on user images stack
    The approach is to have user images uploaded automatically. Users MUST
    log into their Google account before trying to upload images because
    this way we bolster accountability.
    
    S3 bucket batch upload triggers lambda function that indexes the image
    with dynamodb. Users can retrieve images with two api endpoints, one to
    get specific user images once authenticated, and another to get general
    images. ALL IMAGES ARE PUBLIC.

--- infra/lib/user-images-stack.ts
+++ infra/lib/user-images-stack.ts
@@ -1,12 +1,9 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as s3 from "aws-cdk-lib/aws-s3";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as path from "path";
 
 /* STACK MEANT TO STORE IMAGES FOR THIS WEBSITE
  *
@@ -15,48 +12,56 @@ import * as path from "path";
  * cloudFrontDistribution: AWS CloudFront
  * */
 export class AlphanumericDetectorImagesStack extends cdk.Stack {
-        /**
-         * @param {Construct} scope
-         * @param {string} id
-         * @param {StackProps=} props
-         */
-        constructor(scope: Construct, id: string, props: cdk.StackProps) {
-                super(scope, id, props);
+    /**
+     * @param {Construct} scope
+     * @param {string} id
+     * @param {StackProps=} props
+     */
+    constructor(scope: Construct, id: string, props: cdk.StackProps) {
+        super(scope, id, props);
 
-                /* S3 Bucket will store all the images that the users upload. Images are stored under the path ROOT/user/[imagename].png. All images are publicly accessible. */
-                const userImagesBucket = new s3.Bucket(this, "PortfolioWebsiteImagesBucket", {
-                        publicReadAccess: false,
-                        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-                        accessControl: s3.BucketAccessControl.PRIVATE,
-                        removalPolicy: cdk.RemovalPolicy.RETAIN,
-                });
+        /* S3 Bucket will store all the images that the users upload. Images are stored under the path ROOT/user/[imagename].png. All images are publicly accessible. */
+        const userImagesBucket = new s3.Bucket(this, "AlphanumericDetectorUserImagesBucket", {
+            publicReadAccess: false,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            accessControl: s3.BucketAccessControl.PRIVATE,
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+        });
 
-                /* Allows browser access to the bucket */
-                userImagesBucket.addCorsRule({
-                        allowedOrigins: ["*"],
-                        allowedMethods: [s3.HttpMethods.GET],
-                        allowedHeaders: ["*"],
-                });
+        /* Allows browser access to the bucket */
+        userImagesBucket.addCorsRule({
+            allowedOrigins: ["*"],
+            allowedMethods: [s3.HttpMethods.GET],
+            allowedHeaders: ["*"],
+        });
 
-                // CloudFront will cache these images and distribute them appropriate. Since
-                // this service comes with AWS Shield Standard included, it should prevent DDoS attacks. Images are distributed publicly. ANYONE CAN ACCESS THEM BECAUSE THEY ARE NOT MEANT TO BE HIDDEN.
-                const cloudFrontDistribution = new cloudfront.Distribution(this, "PortfolioWebsiteImagesCDN", {
-                        defaultBehavior: {
-                                origin: origins.S3BucketOrigin.withOriginAccessControl(userImagesBucket),
-                                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-                                allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
-                                cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
-                                responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS,
-                        }
-                });
+        // CloudFront will cache these images and distribute them appropriate. Since
+        // this service comes with AWS Shield Standard included, it should prevent DDoS attacks. Images are distributed publicly. ANYONE CAN ACCESS THEM BECAUSE THEY ARE NOT MEANT TO BE HIDDEN.
+        const cloudFrontDistribution = new cloudfront.Distribution(this, "PortfolioWebsiteImagesCDN", {
+            defaultBehavior: {
+                origin: origins.S3BucketOrigin.withOriginAccessControl(userImagesBucket),
+                viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+                cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+                responseHeadersPolicy: cloudfront.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS,
+            }
+        });
 
-                // TODO: We need a dynamoDB table to index the images that each user has.
-                //
-                // TODO: Add API gateway endpoints for executing lambda functions to index a user addition
-                //
-                // TODO: Add lambda functions that index user addition
-                //
-                // TODO: figure out how the indexing is going to work. Do some research on how people usually index their images in S3 with DynamoDB table.
+        // TODO: We need a dynamoDB table to index the images that each user has.
+        /* DynamoDB table to store the users */
+        const userImagesIndexTable: dynamodb.TableV2 = new dynamodb.TableV2(this, "AlphanumericDetectorUserDataTable", {
+            partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
+            contributorInsights: false, // prevent charges
+            tableClass: dynamodb.TableClass.STANDARD,
+        });
+        /* --- */
 
-        };
+        //
+        // TODO: Add API gateway endpoints for executing lambda functions to index a user addition
+        //
+        // TODO: Add lambda functions that index user addition
+        //
+        // TODO: figure out how the indexing is going to work. Do some research on how people usually index their images in S3 with DynamoDB table.
+
+    };
 };
